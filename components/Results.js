c967cd307674ea9a60587f78c0dcc625752c68b8
Clifford <cliffordosei222@gmail.com>
2022-06-23 15:30:50 +0000

    reponsiveness on mediumscreen

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import FlipMove from "react-flip-move";
 
 const Results = ({requests}) => {
   return (
-    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-3 xl:grid-cols-4
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
     3xl:flex flex-wrap justify-center">
         {requests.map((item) => (
             <Thumbnail key={item.id} result={item} />
