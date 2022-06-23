f1989e92558097ac44677149155ca8e2190e7040
Clifford <cliffordosei222@gmail.com>
2022-06-23 15:08:00 +0000

    initial commit

---
+++ components/Results.js
@@ -0,0 +1,15 @@
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
+
+const Results = ({requests}) => {
+  return (
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-3 xl:grid-cols-4
+    3xl:flex flex-wrap justify-center">
+        {requests.map((item) => (
+            <Thumbnail key={item.id} result={item} />
+        ))}
+    </FlipMove>
+  )
+}
+
+export default Results
